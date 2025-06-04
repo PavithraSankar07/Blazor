320089cdda344bbaca5a007d12243d9ed0fe89c7
PavithraSankar07 <pavithra.sankar@syncfusion.com>
2025-06-04 23:31:45 +0530

    Committing the changes of created Online Medical Store application, added classes ,added login page module

--- MedicalStore/Models/UserInfo.cs
+++ MedicalStore/Models/UserInfo.cs
@@ -7,12 +7,13 @@ namespace MedicalStore.Models
 {
     public class UserInfo
     {
-        
-public string UserID { get; set; }
+
+        public string UserID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string MobileNumber { get; set; }
+        public string WalletBalance { get; set; }
 
     }
 }
