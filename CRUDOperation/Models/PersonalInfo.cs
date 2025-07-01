89a46b322fc9f4471d90a6f2b521eba1c0f73bfb
PavithraSankar07 <pavithra.sankar@syncfusion.com>
2025-07-01 09:20:08 +0530

    Committing the changes of adding validations and popup message of adding and delete functionality.

--- CRUDOperation/Models/PersonalInfo.cs
+++ CRUDOperation/Models/PersonalInfo.cs
@@ -1,40 +1,59 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
 
 namespace CRUDOperation.Models
 {
     public class PersonalInfo
     {
-        // private static int s_id = 1;
-        // [Required]
         public int ID { get; set; }
-        // [Range(47, 99, ErrorMessage = "ID must be between 47 and 100.")]
+
+        [Required]
+        [RegularExpression(@"^[0-9]{10}",ErrorMessage ="Number")]
         public long Mobile { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage ="Enter the valid name")]
         public string Name { get; set; }
+
+        [Required]
+        // [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,")]
+        [EmailAddress(ErrorMessage = "Invalid EmailID")]
         public string MailID { get; set; }
+
+        [Required(ErrorMessage = "Upload a correct format photo")]
         public string Photo { get; set; }
+        [Required]
         public string Degree { get; set; }
+        [Required]
         public string Department { get; set; }
+        [Required]
+        [Range(1, 1000000000, ErrorMessage = "Must be in number")]
         public Double Amount { get; set; }
-        
-        
+        [Required]
+
         public DateTime DOB { get; set; } = System.DateTime.Today;
 
-
+        [Required]
         public string[] SelectedDepartments { get; set; } = Array.Empty<string>();
+
         public enum GenderEnum
         {
             Male,
             Female,
             Other
         }
-
+        [Required]
         public GenderEnum Gender { get; set; }
-
+        [Required]
         public string Time { get; set; }
+        [Required]
         public List<string> Languages { get; set; } = new();
+
+        [Required]
         public DateTime DateTime { get; set; } = System.DateTime.Today;
+        [Required]
         public string File { get; set; }
 
 
